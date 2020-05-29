1779af3325973f645f22174fc2786d8346b2cf40
Jayant Rai <jayant.chamling@outlook.com>
2020-05-29 15:04:47 -0500

    fetched the membership.yml value to the UI using thymleaf adding the model attribute in the controller class

--- config-consumer/src/main/java/com/example/configconsumer/Contorller/ProfileController.java
+++ config-consumer/src/main/java/com/example/configconsumer/Contorller/ProfileController.java
@@ -2,10 +2,12 @@ package com.example.configconsumer.Contorller;
 
 import com.example.configconsumer.Model.MemberProfileConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class ProfileController {
 
     @Autowired
@@ -13,7 +15,12 @@ public class ProfileController {
 
 
     @RequestMapping("/profile")
-    public MemberProfileConfiguration getConfig() {
-        return memberProfileConfiguration;
+    public String getConfig(Model model) {
+        model.addAttribute("model", memberProfileConfiguration.getDefaultModel());
+        model.addAttribute("min", memberProfileConfiguration.getMinRentPeriod());
+        return "mprofile";
+
     }
 }
+
+
